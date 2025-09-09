58af74e339f35ad6eb0eb92b2cd086c526553448
Lichagina Kristina <lichagina.kristina07@gmail.com>
2025-09-10 02:29:36 +0300

    sorting

---
+++ Sorting/I.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <string>
+
+void isAnagram(std::string first_word, std::string second_word) {
+    
+}
+
+int main() {
+    std::string first_word, second_word;
+
+    std::cin >> first_word;
+    std::cin >> second_word
+
+
+}
